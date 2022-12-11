30b53737ae10163d8ebbe88e8980096148920c9f
“tickook” <tickoo.k@northeastern.edu>
2022-12-10 23:49:22 -0500

    aws setup

--- src/components/uploadImageAws/index.js
+++ src/components/uploadImageAws/index.js
@@ -1,12 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
+import AWS from 'aws-sdk'
+
+const S3_BUCKET ='careergenie';
+const REGION ='us-east-2';
+const IMAGE_URL = 'https://' + S3_BUCKET + '.s3.' + REGION + '.amazonaws.com/';
+
+
+AWS.config.update({
+    accessKeyId: 'AKIA4FLKLUIPT4L4WXUU',
+    secretAccessKey: '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'
+})
+
+const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET},
+    region: REGION,
+})
 
 const UploadImageAws = () => {
-  return (
-    <>
-      <input type="file" />
-      {/*<button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>*/}
-    </>
-  );
+  const [progress , setProgress] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileInput = (e) => {
+      setSelectedFile(e.target.files[0]);
+  }
+
+  useEffect(() => {
+      if(selectedFile !== null){
+          const params = {
+              ACL: 'public-read',
+              Body: selectedFile,
+              Bucket: S3_BUCKET,
+              Key: selectedFile.name
+          };
+
+          myBucket.putObject(params)
+              .on('httpUploadProgress', (evt) => {
+                  setProgress(Math.round((evt.loaded / evt.total) * 100))
+              })
+              .send((err) => {
+                  if (err) console.log(err)
+              })
+          localStorage.setItem('imageUrl', 'https://careergenie.s3.us-east-2.amazonaws.com/'+ selectedFile["name"]);
+      }
+  }, [selectedFile])
+
+  return( <>
+      <input type="file" onChange={handleFileInput}/>
+  </>);
 };
 
 export default UploadImageAws;
