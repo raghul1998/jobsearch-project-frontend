8b474cb0b4a7b41c176f8d1be7b451e7d8561905
raghul suresh <raghul1998@gmail.com>
2022-12-11 01:05:34 -0500

    first

---
+++ src/components/landing/recentjobmap.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import "./landing.css"
+
+const RecentJobMap = (job) => {
+    const navigate = useNavigate();
+
+    const navigateToLogin= () => {
+        navigate("/login");
+    }
+
+    return (
+        <>
+            <div className="row schedule-item">
+                <div className="col-md-10 col-sm-9">
+                    <div className=" col-2 col-sm-1 speaker">
+                        <img src={job["image"]} alt="job images"/>
+                    </div>
+                    <div className="col-10 col-sm-11">
+                        <h5>
+                            Job Name
+                            <span>
+                                {
+                                    job["eventName"]
+                                }
+                            </span>
+                        </h5>
+                        <p className="float-start">
+                            {job["shortDescription"]}
+                        </p>
+                    </div>
+                </div>
+
+                <div className="col-md-2 col-sm-1">
+                    <br/>
+                    <span className="buy-tickets"
+                       onClick={() => navigateToLogin()}>
+                        Apply Here
+                    </span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RecentJobMap;
---
+++ src/components/landing/recentjobs.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {getLatestPosts} from "../../service/eventService";
+import RecentJobMap from "./recentjobmap";
+
+const RecentJobs = () => {
+    const [jobs, setJobs] = new useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getData() {
+            const jobs = await getLatestPosts();
+            setJobs(jobs);
+        }
+        getData();
+    }, [])
+
+    return (
+        <>
+            {
+                jobs != null &&
+                <div className="tab-content row justify-content-center">
+                    <div role="panel" className="col-lg-9 tab-pane fade show active" id="jobRole">
+                        {
+                            jobs.map(job => <RecentJobMap key={job['_id']} job={job}/>)
+                        }
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+
+export default RecentJobs;
